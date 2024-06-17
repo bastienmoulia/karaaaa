97e5babd9e5c718fc8921ddcecbe50b4eb27e7ff
Bastien <bmoulia@gmail.com>
2024-06-17 22:29:22 +0200

    home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,47 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  inject,
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  private _router = inject(Router);
+
+  joinForm = new FormGroup({
+    code: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(6),
+    ]),
+  });
+
+  ngOnInit(): void {
+    this.joinForm.get('code')!.valueChanges.subscribe((value) => {
+      this.joinForm.get('code')!.setValue(value!.toUpperCase(), {
+        emitEvent: false,
+      });
+    });
+  }
+
+  onSubmit() {
+    if (this.joinForm.valid) {
+      this._router.navigate([this.joinForm.value.code]);
+    }
+  }
+}
